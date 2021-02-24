ad7d368fb20e41a4b138a5b447b9f7e213963ac6
IanaLit <litvinova.iana@gmail.com>
2021-02-24 14:12:38 +0700

    feat: add check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let isClose = false;
+    if (!(str.length % 2 === 0)) {
+        return isClose;
+    } else {
+        let array = str.split("");
+        return checkScope(array);
+    }
+    function checkScope(array) {
+        array.forEach((element, i) => {
+            bracketsConfig.forEach((bracket) => {
+                if (element === bracket[1] && array[i - 1] === bracket[0]) {
+                    array.splice(i - 1, 2);
+                    if (array.length === 0) {
+                        isClose = true;
+                    } else checkScope(array);
+                }
+            });
+        });
+
+        return isClose;
+    }
+};
